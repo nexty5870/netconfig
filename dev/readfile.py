bd92224bc305f57e4868e62d7729306fcf304e09
nexty5870 <Wcm1744Obi>
2019-10-31 17:14:09 +0100

    adding HW formatting and subprocess

--- dev/readfile.py
+++ dev/readfile.py
@@ -1,8 +1,15 @@
 import os
 from bs4 import BeautifulSoup
 import requests
+import subprocess
 
 file = open("config", "r")
+
+#maclist formater
+def str_grouper(n, iterable):
+     args = [iter(iterable)] * n
+     for part in zip(*args): #itertools.izip in 2.x for efficiency.
+         yield "".join(part)
 
 # Repeat for each song in the text file
 for line in file:
@@ -22,16 +29,22 @@ for line in file:
 
     # Convert the mac list to string
     maclist = ''.join(mac)
+    maclistformated = ':'.join(str_grouper(2,mac))
+
     #print(maclist)
 
     source = requests.get('https://hwaddress.com/?q=' + maclist).text
     soup = BeautifulSoup(source, 'lxml')
     vendor = soup.find('div', class_='table-responsive')
     company = vendor.a.text
+    defineip = {}
+    defineip = '/sbin/ip neigh | sed -nr ' + maclistformated +' "/Is/ dev.*$//p"'
+    subprocess.call(defineip,shell=True)
     print('Mac found vendor is: '+company)
     while company == 'Ruckus Wireless':
         print('Ruckus subroutine')
-        print('The switch mac is: ' + mac + ' his management IP is: ' + management + ' Uplink: ' + uplink + ' hostname: ' + hostname + ' having AP start at port: ' + apstart + ' to ' + apend)
+        print(defineip)
+        print('The switch mac is: ' + maclistformated + ' his management IP is: ' + management + ' Uplink: ' + uplink + ' hostname: ' + hostname + ' having AP start at port: ' + apstart + ' to ' + apend)
         break
     else:
         print('Vendor not supported')
